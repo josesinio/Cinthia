b2b334bf381b6a1f7effe554fe59f801f5570ec5
mijalsilvera <jose.cruzet12d1@gmail.com>
2024-01-11 15:06:50 -0300

    update

--- src/Index.ts
+++ src/Index.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response, NextFunction } from "express";
+import { io } from "socket.io-client";
 
-export  default express
   const { Server } = require('socket.io');
   const path = require('path');
   const http = require('http');
@@ -9,11 +9,12 @@ export  default express
 
   const app = express();
   const server = http.createServer(app);
-  const io =  new Server(server);
+  const serverio =  io();
+  const ioserver  = new Server(server);
   const port = 3000;
 
-  app.use(cors)
-  app.use( express.static(path.join(__dirname, 'dist')));
+  app.use(cors())
+  app.use( express.static(path.join('dist')));
   app.use(express.static(path.join( 'public')));
 
   app.use((req: Request, res : Response, next: NextFunction) => {
@@ -27,25 +28,9 @@ export  default express
   app.get('/', (req : Request, res: Response) => {
     res.send('Hola que tal!');
     res.sendFile(path.join( 'public', 'Index.html'))
+    res.sendFile(path.join( 'dist', 'Index.js'))
   
   });
-
-   function MandarMensaje() {
-    const mensajeInput = document.getElementById('message-input') as HTMLInputElement;
-    const mensaje = mensajeInput.value.trim();
-
-    if(mensaje)
-    {
-      server.emit('chat message',mensaje)
-
-      mensajeInput.value = '';
-    }
-  }
-  // Agrega esto después de configurar express.static
-    console.log('Ruta de archivos estáticos:', path.join( 'public'));
-    console.log('Ruta de archivos estáticos:', path.join( 'dist'));
-
-
   server.on('chat massage', (mensaje : string)=> {
     const mensajeChat = document.getElementById('chat-messages');
     const nuevoMensaje = document.createElement('div');
@@ -54,8 +39,15 @@ export  default express
     mensajeChat.appendChild(nuevoMensaje);
     }
   })
+  //server.document.getElementById('myButton').addEventListener('click', MandarMensaje);
 
-  io.on('connection', (socket  = io('http://localhost:3000'))  => {
+
+  // Agrega esto después de configurar express.static
+    console.log('Ruta de archivos estáticos:', path.join( 'public'));
+    console.log('Ruta de archivos estáticos:', path.join( 'dist'));
+
+
+    ioserver.on('connection', (socket  = io('http://localhost:3000'))  => {
 
     console.log("El usuario se ha  conectado.");
 
@@ -66,6 +58,17 @@ export  default express
       console.log("El usuarios se desconecto.");
     });
   });
+  export async function MandarMensaje() {
+    const mensajeInput = document.getElementById('message-input') as HTMLInputElement;
+    const mensaje = mensajeInput.value.trim();
+
+    if(mensaje)
+    {
+      serverio.emit('chat message',mensaje)
+
+      mensajeInput.value = '';
+    }
+  }
 
   server.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
